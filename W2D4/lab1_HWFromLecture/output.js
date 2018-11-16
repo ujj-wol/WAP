fb502599d42616805245397b4d2e6eda860cf9a8
Ujjwol Bhandari <ubhandari2@pc-uzol.local>
2018-11-16 12:43:19 -0600

    cleaning

--- W2D4/lab1_HWFromLecture/output.js
+++ W2D4/lab1_HWFromLecture/output.js
@@ -1,9 +1,10 @@
+(function (){
 "use strict";
 
 $(function() {
 	$('#hw').change(function() {
 		//var file = 'homeworks/'+$('#hw').val();
-		//var file = 'http://www.mumstudents.org/~rshrestha/homework-ajax/homeworks/' + $('#hw').val();
+		//var file = '/~rshrestha/homework-ajax/homeworks/' + $('#hw').val();  //this will also work
 		var file = $('#hw').val();
 		$.ajax({
 			'url': file,
@@ -23,3 +24,5 @@ function ajaxFailure(xhr, status, exception) {
 }
 
 $.get('https://www.google.com/images/branding/product/ico/googleg_lodp.ico');
+
+})();
